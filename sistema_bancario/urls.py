ff952c7b6be57e7ec2613ad29db3c09b0b1e7797
gerardo-pz <perez.gerardo1108@gmail.com>
2025-06-25 18:51:30 -0500

    Update urls.py

--- sistema_bancario/urls.py
+++ sistema_bancario/urls.py
@@ -1,22 +1,8 @@
-"""
-URL configuration for sistema_bancario project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from usuarios.views import landing_page
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', landing_page, name='landing_page'),
@@ -27,4 +13,6 @@ urlpatterns = [
     path('auditoria/', include('auditoria.urls')),
 ]
 
+# Esto va fuera del listado principal
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
